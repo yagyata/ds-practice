ea8f953b58bda70a8aa27e36758be4455a97f5a9
Yagyata <yagyatasharmaa@gmail.com>
2025-05-12 14:39:01 +0530

    [Yagyata] ADD: Insert a node at the end of the linked list

---
+++ day4/InsertAtTail.java
@@ -0,0 +1,52 @@
+package day4;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class InsertAtTail {
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+
+        public SinglyLinkedList() {
+            this.head = null;
+        }
+
+
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+
+    static SinglyLinkedListNode insertNodeAtTail(SinglyLinkedListNode head, int data) {
+        SinglyLinkedListNode newNode = new SinglyLinkedListNode(data);
+        if(head == null){
+            head = newNode;
+            return head;
+        }
+        SinglyLinkedListNode temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+}
